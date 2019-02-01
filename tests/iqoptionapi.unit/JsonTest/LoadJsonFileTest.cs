c528d52212de3944055b1578182636aaf78bbf32
Mongkon Eiadon <leiadon@brandnewday.nl>
2019-02-01 19:06:19 +0700

    rebranding new tests

--- tests/iqoptionapi.unit/JsonTest/LoadJsonFileTest.cs
+++ tests/iqoptionapi.unit/JsonTest/LoadJsonFileTest.cs
@@ -1,25 +1,18 @@
 ﻿using NUnit.Framework;
 using System;
 using Newtonsoft.Json;
-using TestsFor;
 
 namespace IqOptionApi.unit.JsonTest {
 
     [TestFixture]
-    public abstract class LoadJsonFileTest<TType> : Test {
+    public abstract class LoadJsonFileTest<TType> {
 
         public abstract string JsonSourceFileName { get; }
 
         private Lazy<TType> _LazySource => new Lazy<TType>(() => JsonConvert.DeserializeObject<TType>(LoadJson()));
 
         public TType ReadFileSource() => _LazySource.Value;
-
-        [SetUp]
-        public void SetUp()
-        {
-
-        }
-
+        
         public string LoadJson() {
 
             var JSON = System.IO.File.ReadAllText($"Json\\{JsonSourceFileName}");
