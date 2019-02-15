3c4d3e5a2ccee966fa7c590ddc9369ae54d9d681
Mongkon Eiadon <leiadon@brandnewday.nl>
2019-02-15 17:37:32 +0700

    add circle.yml

---
+++ src/IqOptionApi/Models/Enums/BuyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace IqOptionApi.Models
+{
+    public enum EnumBuyType
+    {
+        [EnumMember(Value = "turbo")]
+        Turbo,
+        [EnumMember(Value = "binary")]
+        Binary
+    }
+}
