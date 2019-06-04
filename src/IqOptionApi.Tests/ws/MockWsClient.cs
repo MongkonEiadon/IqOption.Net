0771c2151daae393d08ffa5d71525df5a46c7e9d
Mongkon Eiadon <mongkon.eiadon@hotmail.com>
2019-06-04 16:25:03 +0700

    update packages

--- src/IqOptionApi.Tests/ws/MockWsClient.cs
+++ src/IqOptionApi.Tests/ws/MockWsClient.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Reactive.Concurrency;
 using System.Reactive.Subjects;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Reactive.Testing;
 using Websocket.Client;
 
 namespace IqOptionApi.Tests.ws {
     public class MockWsClient : IWebsocketClient {
+        private IObservable<ResponseMessage> _messageReceived;
+
         public MockWsClient() {
             var scheduler = new TestScheduler();
             scheduler.Schedule(TimeSpan.FromTicks(20), () => Start());
@@ -39,12 +42,18 @@ namespace IqOptionApi.Tests.ws {
             return Task.CompletedTask;
         }
 
+        public Uri Url { get; set; }
+
         public IObservable<string> MessageReceived => _subject;
         public int ReconnectTimeoutMs { get; set; }
         public int ErrorReconnectTimeoutMs { get; set; }
         public string Name { get; set; } = nameof(MockWsClient);
         public bool IsStarted { get; private set; }
         public bool IsRunning { get; private set; }
+        public Encoding MessageEncoding { get; set; }
+
+        IObservable<ResponseMessage> IWebsocketClient.MessageReceived => _messageReceived;
+
         public IObservable<ReconnectionType> ReconnectionHappened { get; }
         public IObservable<DisconnectionType> DisconnectionHappened { get; }
     }
